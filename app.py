3dbb8437d94ece7aec5c32165578e3fd8446d354
David Boadita <dlboadita.yc@gmail.com>
2026-06-01 11:03:38 -0700

    Display analysis results section

--- app.py
+++ app.py
@@ -9,16 +9,21 @@ def home():
 
    meeting_notes = ""
    
+   analysis = ""
+   
    if request.method == "POST":
 
     meeting_notes = request.form["meeting_notes"]
 
     print(meeting_notes)
 
+    analysis = "This is a mock analysis."
+
    return render_template(
       "index.html",
-      meeting_notes=meeting_notes
+      meeting_notes=meeting_notes,
+      analysis=analysis
    )
-   
+
 if __name__ == "__main__":
     app.run(debug=True)
