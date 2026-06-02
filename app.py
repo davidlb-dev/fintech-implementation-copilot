f40a8b015e29b3b429ea3f3371bb9ecf8c3ad8f4
David Boadita <dlboadita.yc@gmail.com>
2026-06-01 18:22:06 -0700

    Integrate OpenAI API for meeting note analysis

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
+from datetime import datetime
 from flask import Flask, render_template, request
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
 app = Flask(__name__)
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=api_key)
+
+print(
+    "Flask startup:",
+    datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    "| API key loaded:",
+    api_key is not None
+)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -13,11 +30,19 @@ def home():
    
    if request.method == "POST":
 
-    meeting_notes = request.form["meeting_notes"]
+      meeting_notes = request.form["meeting_notes"]
 
-    print(meeting_notes)
+   #  print(meeting_notes)
 
-    analysis = "This is a mock analysis."
+      print("Meeting notes:", meeting_notes)
+      print("Meeting notes length:", len(meeting_notes))
+
+      response = client.responses.create(
+      model="gpt-5",
+      input=meeting_notes
+   )
+
+      analysis = response.output_text
 
    return render_template(
       "index.html",
